1bf712fe221bd29fc34ec2595adb852407fc250e
franco3v3ns3n <fr.evensen@duocuc.cl>
2026-05-24 19:12:27 -0400

    Se agrega la estructura para docker

--- estadia/src/main/java/com/veranum/estadia/EstadiaApplication.java
+++ estadia/src/main/java/com/veranum/estadia/EstadiaApplication.java
@@ -2,12 +2,13 @@ package com.veranum.estadia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class EstadiaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EstadiaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EstadiaApplication.class, args);
+    }
 }
