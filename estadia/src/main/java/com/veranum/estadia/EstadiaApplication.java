e8f9aa94ad78bfadb49694d3097f0cdab5baefc9
franco3v3ns3n <fr.evensen@duocuc.cl>
2026-05-24 18:49:28 -0400

    Creacion proyecto estadia

---
+++ estadia/src/main/java/com/veranum/estadia/EstadiaApplication.java
@@ -0,0 +1,13 @@
+package com.veranum.estadia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstadiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstadiaApplication.class, args);
+	}
+
+}
