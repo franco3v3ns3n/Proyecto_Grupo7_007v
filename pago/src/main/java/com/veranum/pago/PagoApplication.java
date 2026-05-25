f3e1732770e4b0c69c38b62308cd11cd5aa50c78
franco3v3ns3n <fr.evensen@duocuc.cl>
2026-05-25 18:35:22 -0400

    Se agrega configuración Docker y propiedades del microservicio

--- pago/src/main/java/com/veranum/pago/PagoApplication.java
+++ pago/src/main/java/com/veranum/pago/PagoApplication.java
@@ -2,12 +2,13 @@ package com.veranum.pago;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class PagoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PagoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PagoApplication.class, args);
+    }
 }
