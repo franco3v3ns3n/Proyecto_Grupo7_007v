515a782cfb22ac5029f4587e397f5a6b695252a6
franco3v3ns3n <fr.evensen@duocuc.cl>
2026-05-25 18:12:19 -0400

    Se agrega configuración Docker y propiedades del microservicio

--- empleado/src/main/java/com/veranum/empleado/EmpleadoApplication.java
+++ empleado/src/main/java/com/veranum/empleado/EmpleadoApplication.java
@@ -2,12 +2,13 @@ package com.veranum.empleado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class EmpleadoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmpleadoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmpleadoApplication.class, args);
+    }
 }
