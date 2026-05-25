9777dc088ce33d50cac3a8afa6902b3c3ee651c6
franco3v3ns3n <fr.evensen@duocuc.cl>
2026-05-25 18:36:21 -0400

    Se agregan clientes externos y manejo global de errores

---
+++ pago/src/main/java/com/veranum/pago/exceptions/RemoteServiceException.java
@@ -0,0 +1,8 @@
+package com.veranum.pago.exceptions;
+
+public class RemoteServiceException extends RuntimeException {
+
+    public RemoteServiceException(String message) {
+        super(message);
+    }
+}
