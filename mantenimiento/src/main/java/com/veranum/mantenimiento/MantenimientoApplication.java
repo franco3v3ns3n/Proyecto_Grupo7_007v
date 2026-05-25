5030658921cff0d0ec78bda037b1f053ca217973
franco3v3ns3n <fr.evensen@duocuc.cl>
2026-05-25 02:30:34 -0400

    Update MantenimientoApplication.java

--- mantenimiento/src/main/java/com/veranum/mantenimiento/MantenimientoApplication.java
+++ mantenimiento/src/main/java/com/veranum/mantenimiento/MantenimientoApplication.java
@@ -2,12 +2,13 @@ package com.veranum.mantenimiento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MantenimientoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MantenimientoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MantenimientoApplication.class, args);
+    }
 }
